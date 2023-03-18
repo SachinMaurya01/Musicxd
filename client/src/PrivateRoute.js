aa807a2c181587b40b4e32b29977d38df043934d
Sachin Maurya <100995198+SachinMaurya01@users.noreply.github.com>
2023-03-18 09:44:17 +0530

    Update PrivateRoute.js

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,28 +1,16 @@
 import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, user, ...rest }) => {
-	const styles = {
-		padding: "6rem 0 0 26rem",
-		backgroundColor: "#181818",
-		color: "#ffffff",
-		minHeight: "calc(100vh - 6rem)",
-	};
+	const styles = 
+	      { padding: "6rem 0 0 26rem", backgroundColor: "#181818", color: "#ffffff", minHeight: "calc(100vh - 6rem)", };
 
 	return (
-		<Route
-			{...rest}
-			render={(props) =>
-				user ? (
-					<div style={styles}>
-						<Component {...props} />
-					</div>
-				) : (
-					<Redirect
-						to={{ pathname: "/login", state: { from: props.location } }}
-					/>
-				)
-			}
-		/>
+		<Route {...rest} render={(props) =>
+				user ? 
+					( <div style={styles}> <Component {...props} /> </div>  ) 
+				     : 
+					( <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+				)} />
 	);
 };
 
