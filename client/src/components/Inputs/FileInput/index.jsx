4619f941e74bbb776bfc6b7c157494ed3672124a
Sachin Maurya <100995198+SachinMaurya01@users.noreply.github.com>
2023-03-18 09:51:32 +0530

    Update index.jsx

--- client/src/components/Inputs/FileInput/index.jsx
+++ client/src/components/Inputs/FileInput/index.jsx
@@ -15,31 +15,17 @@ const FileInput = ({
 
 	return (
 		<div className={styles.container}>
-			<input
-				type="file"
-				ref={inputRef}
-				onChange={(e) => handleInputState(name, e.currentTarget.files[0])}
-				vlaue={value}
-				{...rest}
-			/>
+			<input type="file" ref={inputRef} onChange={(e) => handleInputState(name, e.currentTarget.files[0])}
+				value={value} {...rest} />
 			<Button
-				style={{
-					width: "15rem",
-				}}
+				style={{ width: "15rem", }}
 				onClick={() => inputRef.current.click()}
-				label={label}
-			/>
+				label={label} />
 			{type === "image" && value && (
-				<img
-					src={typeof value === "string" ? value : URL.createObjectURL(value)}
-					alt="file"
-				/>
+				<img src={typeof value === "string" ? value : URL.createObjectURL(value)} alt="file" />
 			)}
 			{type === "audio" && value && (
-				<audio
-					src={typeof value === "string" ? value : URL.createObjectURL(value)}
-					controls
-				/>
+				<audio src={typeof value === "string" ? value : URL.createObjectURL(value)} controls />
 			)}
 			<Button label="Upload" style={{ width: "10rem" }} />
 		</div>
